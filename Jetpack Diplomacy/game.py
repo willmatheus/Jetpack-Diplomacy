89ab212246d5f700ff5d0eb27908df18f16a8fab
William Matheus <108390581+willy346@users.noreply.github.com>
2023-02-22 06:03:50 -0400

    menu screen completed
    we have to finish the interaction of menu screen with player, and start to work on gameplay

--- Jetpack Diplomacy/game.py
+++ Jetpack Diplomacy/game.py
@@ -4,15 +4,24 @@ from config import *
 class Game:
     def __init__(self, screen):
         self.screen = screen
-        self.background = scenario1
+        self.menu_looping = menu_looping
+        self.char_looping = char_looping
+        self.background = start_img_menu
+        self.background2 = char_img_menu
         self.players_sprites = pygame.sprite.Group()
         self.players = []
 
     def get_screen(self):
-        self.background = scenario1
+        self.background = start_img_menu
 
     def game_loop(self):
         self.get_screen()
+
+        while looping:
+            self.check_events()
+            self.draw_sprites()
+            pygame.display.update()
+            clk.tick(fps)
 
     def check_events(self):
         clk.tick(60)
@@ -21,5 +30,8 @@ class Game:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-        self.check_events()
-        pygame.display.update()
+
+
+    def draw_sprites(self):
+        screen.blit(self.background, (0, 0))
+        
