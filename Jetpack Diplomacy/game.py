4641901546c826036c43b13bbf7002571e26da76
paulovictormourao <mouraopaulovictor@gmail.com>
2023-02-21 20:10:32 -0400

    menu inicio, não funcional.

--- Jetpack Diplomacy/game.py
+++ Jetpack Diplomacy/game.py
@@ -1,8 +1,11 @@
+import pygame
 from config import *
+from pygame import locals
 
 
 class Game:
     def __init__(self, screen):
+        self.menu_looping = menu_looping
         self.screen = screen
         self.background = scenario1
         self.players_sprites = pygame.sprite.Group()
@@ -11,8 +14,10 @@ class Game:
     def get_screen(self):
         self.background = scenario1
 
-    def game_loop(self):
-        self.get_screen()
+    def start_menu(self):
+        if menu_looping:
+            screen.blit(start_img_menu, (0, 0))
+            pygame.display.update()
 
     def check_events(self):
         clk.tick(60)
@@ -21,5 +26,14 @@ class Game:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
+
         self.check_events()
         pygame.display.update()
+
+    def game_loop(self):
+        self.get_screen()
+
+        while looping:
+
+            self.start_menu()
+            self.check_events()
--- Jetpack Diplomacy/main.py
+++ Jetpack Diplomacy/main.py
@@ -1,8 +1,11 @@
 from config import *
 from game import Game
 
+
 pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Jetpack-Diplomacy")
 play = Game(screen)
+
+
 play.game_loop()
