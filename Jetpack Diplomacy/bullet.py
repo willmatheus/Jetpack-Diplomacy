3d4f16487754495b26049f647e215bbbf49f7105
William Matheus <108390581+willy346@users.noreply.github.com>
2023-02-25 18:09:40 -0400

    bullet changes

--- Jetpack Diplomacy/bullet.py
+++ Jetpack Diplomacy/bullet.py
@@ -2,22 +2,11 @@ from config import *
 
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, player, vel_x, vel_y):
-        super().__init__()
-        self.player = player
-        self.x = player.rect.x
-        self.y = player.rect.y
-        self.image = bullet
+    def __init__(self, x, y, direction):
+        pygame.sprite.Sprite.__init__(self)
+        self.speed = 10
+        self.image = bullet_img
         self.image = pygame.transform.scale(self.image, (8 * 5, 8 * 5))
-        self.group = pygame.sprite.Group()
         self.rect = self.image.get_rect()
-        self.rect.center = [self.x, self.y]
-        self.dx = vel_x
-        self.dy = vel_y
-
-    def update(self):
-        self.move()
-
-    def move(self):
-        self.rect.x += self.dx
-        self.rect.y += self.dy
+        self.rect.center = (x, y)
+        self.direction = direction
