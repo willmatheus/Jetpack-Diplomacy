10c555d05bb12be2d284e89996611a77d044b984
William Matheus <108390581+willmatheus@users.noreply.github.com>
2023-04-05 04:18:08 -0400

    Delete Jetpack-Diplomacy directory

--- Jetpack-Diplomacy/wall.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-
-class Wall(pygame.sprite.Sprite):
-    def __init__(self, color, dimensions, coordinates):
-        super().__init__()
-        self.image = pygame.Surface(dimensions)
-        self.image.fill(color)
-        self.rect = self.image.get_rect(topleft=coordinates)
